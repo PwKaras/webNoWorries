e2e82d66e7284567ea338c019d45583a7312befe
PAWEŁ KARAS <pwkaras@poczta.onet.pl>
2020-03-28 08:53:03 +0100

    web service

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// navigation
+const navigations = document.querySelector('.web-nav');
+
+navigations.addEventListener('click', event => {
+    const navigationMenu = this.document.querySelector('.nav-list');
+    navigationMenu.classList.toggle('open')
+})
+
+
